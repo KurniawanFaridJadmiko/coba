0bb8e8b7a656ccbf699f8a32de00d8719e93213e
KurniawanFaridJadmiko <faridkojad17@gmail.com>
2023-09-15 06:51:56 +0000

    Hallo hari ini hari Jumat, hari ke 5 magang

--- test.py
+++ test.py
@@ -1,2 +1,8 @@
+# Program Sapaan Selamat Datang
+def sapaan_selamat_datang():
+    print("Selamat datang di program ini!")
+    nama = input("Masukkan nama Anda: ")
+    print(f"Selamat datang, {nama}!")
 
-print("Halo, dunia!")
+# Panggil fungsi sapaan_selamat_datang
+sapaan_selamat_datang()
