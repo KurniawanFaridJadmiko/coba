15b8a8757ef4dc695df54f744dd55457310aff26
KurniawanFaridJadmiko <faridkojad17@gmail.com>
2023-09-15 08:06:34 +0000

    Ini hanya percobaan saja, percobaan yang sudah beberapa kali

--- test.py
+++ test.py
@@ -1,8 +1,25 @@
-# Program Sapaan Selamat Datang
-def sapaan_selamat_datang():
-    print("Selamat datang di program ini!")
-    nama = input("Masukkan nama Anda: ")
-    print(f"Selamat datang, {nama}!")
+def tambah(a, b):
+    return a + b
 
-# Panggil fungsi sapaan_selamat_datang
-sapaan_selamat_datang()
+def kurang(a, b):
+    return a - b
+
+def kali(a, b):
+    return a * b
+
+def bagi(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Tidak bisa membagi dengan nol."
+
+hasil_penjumlahan = tambah(5, 3)
+hasil_pengurangan = kurang(7, 2)
+hasil_perkalian = kali(4, 6)
+hasil_pembagian = bagi(8, 2)
+
+print(f"Hasil penjumlahan: {hasil_penjumlahan}")
+print(f"Hasil pengurangan: {hasil_pengurangan}")
+print(f"Hasil perkalian: {hasil_perkalian}")
+print(f"Hasil pembagian: {hasil_pembagian}")
+
